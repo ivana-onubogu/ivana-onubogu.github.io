594a258ec4ec6e26ba259415feafa35d00fbb78f
ivana-onubogu <ivanaionubogu@gmail.com>
2023-12-27 15:24:34 -0500

    commit2_video

--- src/flowfield.js
+++ src/flowfield.js
@@ -454,3 +454,59 @@ function revertW()
 	w_pressed = false;
 	strokeWeightEffect = strokeWeightDefault;
 }
+// USER RECORDING
+const videoButton = document.getElementById('main_video-button');
+const video = document.getElementbyID('main_video')
+
+let mediaRecorder;
+
+videoButton.onclick = () => {
+	switch (videoButton.textContent) {
+		case 'Record':
+				videoButton.textContent = 'Stop';
+				startRecording();
+				break;
+		case 'Stop':
+				videoButton.textContent = 'Record';
+				stopRecording();
+				break;
+	}
+}
+
+//async function - starts web camera; declared using 'async' keyword; always return a promise (which would be the user input)
+async function init(){
+	try{
+	const stream = await navigator.mediaDevices.getUserMedia({
+		audio: true,
+		video: true
+	});
+	startWebCamera(stream);		
+} catch (err) {
+	console.log('Error retrieving media device');
+	console.log(err);
+}
+}
+function startWebCamera(stream) {
+	video.srcObject = stream;
+	window.stream = stream;
+}
+function startRecording() {
+	if (video.srcObject === null) {
+		video.srcObject = window.stream;
+		mediaRecorder = new MediaRecorder(window.stream, {mimeType: 'video/webm;codecs=vp9.opus'});
+		mediaRecorder.start();
+		mediaRecorder.ondataavailable = recordVideo;
+	}
+}
+function recordVideo(event){
+	if (event.data && event.data.size > 0){
+		video.srcObject = null;
+		let videoUrl = URL. createObjectURL(event.data);
+		video.src = videoUrl;
+	}
+}
+
+function stopRecording(){
+	mediaRecorder.stop();
+}
+init();
